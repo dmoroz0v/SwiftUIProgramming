e7c077427403569d9fafb505b35498237a27fd6b
aarmandiyev <aarmandiyev@yandex-team.ru>
2023-12-12 17:45:50 +0500

    revision of taxi animation

--- MyApp.swift
+++ MyApp.swift
@@ -4,22 +4,37 @@ import SwiftUI
 struct MyApp: App {
     let storage = Storage()
     @State var model: ContentViewStore.Model? = nil
+    @State var yOffset: CGFloat = 0.0
+    @State var isSplashScreenExists: Bool = true
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                if let model {
-                    ContentView(store: ContentViewStore(
-                        model: model,
-                        storage: storage
-                    ))
-                }
-                SplashScreen()
-                    .onAppear {
-                        Task {
-                            let selectedPhoto = await storage.getSelectedPhoto()
-                            self.model = .init(selectedPhoto: selectedPhoto, transformedPhotos: [])
-                        }
+            GeometryReader { proxy in
+                ZStack {
+                    if let model {
+                        ContentView(store: ContentViewStore(
+                            model: model,
+                            storage: storage
+                        ))
                     }
+                    if isSplashScreenExists {
+                        SplashScreen()
+                            .animation(.linear(duration: 0.5).delay(2.25), value: yOffset)
+                            .onAppear {
+                                yOffset = -proxy.size.height - proxy.safeAreaInsets.top - proxy.safeAreaInsets.bottom
+                                Task {
+                                    let selectedPhoto = await storage.getSelectedPhoto()
+                                    self.model = .init(
+                                        selectedPhoto: selectedPhoto,
+                                        transformedPhotos: []
+                                    )
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.75) {
+                                    isSplashScreenExists = false
+                                }
+                            }
+                            .offset(y: yOffset)
+                    }
+                }
             }
         }
     }
--- SplashScreen.swift
+++ SplashScreen.swift
@@ -3,29 +3,25 @@ import SwiftUI
 struct SplashScreen: View {
 
     @State var whiteTakesAllHorizontalSpace = false
-    @State var whiteTakesAllVerticalSpace = false
 
     var body: some View {
         VStack(spacing: 0) {
             Color.yellow
-                .frame(maxHeight: whiteTakesAllVerticalSpace ? .zero : .infinity)
             HStack(spacing: 0) {
-                Color.white
-                    .frame(maxHeight: whiteTakesAllVerticalSpace ? .zero : .infinity)
-                    .frame(maxWidth: .infinity)
+                VStack {
+                    Color.white
+                    Text("YandexGo")
+                        .padding(.bottom, 32)
+                }
+                .frame(maxWidth: .infinity)
                 Color.black
                     .frame(maxWidth: whiteTakesAllHorizontalSpace ? .zero : .infinity)
             }
-            Color.clear
-                .frame(maxHeight: whiteTakesAllVerticalSpace ? .infinity : .zero)
         }
         .ignoresSafeArea()
         .onAppear {
             withAnimation(.linear(duration: 0.25).delay(2)) {
                 whiteTakesAllHorizontalSpace = true
-            }
-            withAnimation(.linear(duration: 0.25).delay(2.5)) {
-                whiteTakesAllVerticalSpace = true
             }
         }
     }
